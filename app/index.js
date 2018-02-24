34e777b33ffcdb6c2a8c7abe988eed7b9ef8c23b
shikelong <578622705@qq.com>
2018-02-24 18:14:05 +0800

    change folder structure and add jade template engine

--- app/index.js
+++ app/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const path = require('path')
+const index = require('./routes/index')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('hello world'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.get('/image', (req, res) => res.sendFile(path.join(__dirname, '/assets/image.png')))
+const options = {
+  maxAge: 50000
+}
+app.use('/static', express.static(path.join(__dirname, 'assets'), options))
 
-app.use('/static', express.static(path.join(__dirname, 'assets')))
+
+app.use('/', index)
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
